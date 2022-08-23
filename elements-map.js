15456be9a0f63ef400971ad1b1a7e530a7d835a9
Alexander Goncharuk <alexander.goncharuk@brainbase.com>
2022-08-23 14:51:58 +0300

    feat: add invisible wall element

--- elements-map.js
+++ elements-map.js
@@ -3,6 +3,7 @@ import { Player } from 'player';
 export const ELEMENTS_MAP = {
 	'.': {type: 'empty'},
 	'#': {type: 'wall'},
+	'i': {type: 'wall', modifiers: ['invisible']},
 	'+': {type: 'lava'},
 	'@': {type: Player}
 }
